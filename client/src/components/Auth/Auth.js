b8377e93234aa5697632a4d16b2238c554cb4144
Suhas Jagannath <sjaga003@ucr.edu>
2021-03-17 18:24:32 -0700

    Change Auth CSS to match contact form

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -83,18 +83,19 @@ const Auth = () => {
 };
 
 const Card = styled.div`
-  background: var(--highlight-color);
+  background: var(--card-color);
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-  width: 50rem;
+  width: 60rem;
 
   padding: 2rem;
   color: var(--bg-color);
   font-family: var(--header-font);
   font-size: 3.6rem;
   border-radius: 6px;
+  filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.25));
   margin-top: 10rem;
 `;
 
@@ -105,7 +106,7 @@ const PageContainer = styled.div`
   align-items: center;
   height: 100%;
   margin: 0 auto;
-  padding: 0 15vw;
+  padding: 0vh 15vw 1vh 15vw;
   overflow-x: hidden;
 `;
 
@@ -118,22 +119,23 @@ const UserImage = styled.div`
   font-size: 1.25rem;
   align-items: center;
   flex-shrink: 0;
-  font-family: var(--heading-font);
+  font-family: var(--header-font);
   line-height: 1;
   user-select: none;
   border-radius: 50%;
   justify-content: center;
-  background: var(--bg-color);
+  background: var(--highlight-color);
 
   svg {
-    color: var(--highlight-color);
+    color: var(--bg-color);
     height: 2rem;
   }
 `;
 
 const Title = styled.span`
   font-weight: 600;
-  font-family: var(--heading-font);
+  font-family: var(--header-font);
+  color: var(--header-color);
 `;
 
 const Form = styled.form`
@@ -153,8 +155,9 @@ const Input = styled.input`
   border-radius: 8px;
   color: var(--text-color);
   font-family: var(--text-font);
-  padding-left: 1rem;
   margin-top: 2rem;
+  border: 1px solid lightgray;
+  padding-left: 1rem;
 `;
 
 const InputRow = styled.div`
@@ -172,8 +175,11 @@ const PasswordRow = styled.div`
   width: 100%;
   background: var(--bg-color);
   border-radius: 8px;
+  border: 1px solid lightgray;
   input {
     margin-top: 0;
+
+    border: 0;
   }
   svg {
     color: var(--highlight-color);
@@ -185,23 +191,24 @@ const PasswordRow = styled.div`
 `;
 
 const Button = styled.button`
-  margin-top: 2rem;
-  height: 100%;
-  width: 12rem;
+  border: 0;
+  padding: 1rem 2rem;
+  font-family: var(--text-font);
+  font-size: 1.8rem;
+  cursor: pointer;
+  outline: none;
+  background: transparent;
   background: var(--highlight-color);
   color: white;
-  border: 0;
-  font-family: var(--header-font);
-  font-size: 1.8rem;
-  font-weight: 600;
-  cursor: pointer;
-  border-radius: 6px;
-  outline: 0;
+  border-radius: 4px;
+  margin-top: 2rem;
 `;
 
 const ModeButton = styled(Button)`
   align-self: flex-end;
   width: fit-content;
+  background: none;
+  color: var(--text-color);
 `;
 
 export default Auth;
